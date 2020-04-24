aaca520f0642cff45567b5614e602af4bc60e09e
Raphaël Zumer <rzumer@tebako.net>
2020-04-24 10:49:34 -0400

    Update metrics_gather.sh
    This allows using the system-installed dav1d or aomdec
    binaries instead of requiring the user to set them up in
    a specific local directory.
    
    The aomdec directory was also not valid for non-AOM codecs.

--- metrics_gather.sh
+++ metrics_gather.sh
@@ -93,10 +93,6 @@ if [ -z "$YUV2YUV4MPEG" ]; then
   export YUV2YUV4MPEG="$DAALATOOL_ROOT/tools/yuv2yuv4mpeg"
 fi
 
-if [ -z "$DAV1D" ]; then
-  export DAV1D="$DAALATOOL_ROOT/../dav1d/build/tools/dav1d"
-fi
-
 if [ -z "$CODEC" ]; then
   export CODEC=daala
 fi
@@ -227,7 +223,7 @@ thor-rt)
   ;;
 rav1e)
   $($TIMER $RAV1E $FILE --quantizer $x -o $BASENAME.ivf -r $BASENAME-rec.y4m --threads 1 $EXTRA_OPTIONS > $BASENAME-enc.out)
-  if [ -f "$DAV1D" ]; then
+  if hash dav1d 2>/dev/null; then
     $($TIMERDEC $DAV1D -q -i $BASENAME.ivf -o $BASENAME.y4m) || (echo "Corrupt bitstream detected!"; exit 98)
   else
     $($TIMERDEC $AOMDEC --codec=av1 $AOMDEC_OPTS -o $BASENAME.y4m $BASENAME.ivf) || (echo "Corrupt bitstream detected!"; exit 98)
